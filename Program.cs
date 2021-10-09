3eae2557332a9e9fc0763649792c740735322222
Zineb El houz <92126213+ZinebElhouz@users.noreply.github.com>
2021-10-09 14:52:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NombrePremier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Nb, Test = 0;
+
+            Console.WriteLine("Veuillez entrer un nombre entier : ");
+            Nb = int.Parse(Console.ReadLine());
+
+            if (Nb <= 3)
+                Test = 1;
+            else
+            {
+                int Sqrt_Nb = (int)Math.Sqrt(Nb);
+                for (int i = 2; i <= Sqrt_Nb; i++)
+                {
+                    if (Nb % i == 0)
+                    {
+                        Test = 0;
+                        break;
+                    }
+                    else
+                        Test = 1;
+                }
+            }
+            if (Test != 0)
+            {
+                Console.WriteLine(Nb + " est premier");
+            }
+            else
+            {
+                Console.WriteLine(Nb +" n'est pas premier");
+            }
+        }
+    }
+}
